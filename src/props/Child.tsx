3f82ba2fb1916c2c2e554da9dcac3fa956469aea
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-06-03 21:57:12 +0500

    working with Child and Parrent components

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,7 +1,21 @@
+import React from "react";
+
 interface ChildProps {
     color: string;
+    onClick: () => void;
+    children?: React.ReactNode;
 }
 
-export const Child = ({color}: ChildProps) => {
-    return <div>{color}</div>
+export const Child = ({color, onClick}: ChildProps) => {
+    return <div>
+        {color}
+        <button onClick={onClick}>Click me</button>
+    </div>
 };
+
+export const ChildAsFC: React.FC<ChildProps> = ({color, onClick}) => {
+    return <div>
+        {color}
+        <button onClick={onClick}>Click me</button>
+    </div>
+}
--- src/props/Parrent.tsx
+++ src/props/Parrent.tsx
@@ -1,7 +1,13 @@
-import {Child} from './Child';
+// import {Child} from './Child';
+import {ChildAsFC} from "./Child";
 
 const Parent = () => {
-    return <Child color="red"/>
+    // return <Child color="red" onClick={() => console.log('Clicked')}/>
+    return (
+        <ChildAsFC color="red" onClick={() => console.log('Clicked!')}>
+            hello;
+        </ChildAsFC>
+    )
 }
 
 export default Parent;
